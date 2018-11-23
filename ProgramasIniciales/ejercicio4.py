7b133dbcb0b10f8d98e88b6ae674284c960315b4
Alvaro Sanz Rodrigo <sanzrodrigoalvaro@gmail.com>
2018-11-23 11:42:35 +0100

    Ejercicio 4

--- ProgramasIniciales/ejercicio4.py
+++ ProgramasIniciales/ejercicio4.py
@@ -2,7 +2,7 @@ import math
 
 annoIntroducido = int(input("Introduzca un año \n"))
 
-if (annoIntroducido/4).is_integer() and (annoIntroducido/100).is_integer() and (annoIntroducido/400).is_integer():
+if (annoIntroducido/4).is_integer():
     print(annoIntroducido, " es bisiesto")
 else:
     print(annoIntroducido, " no es bisiesto")
