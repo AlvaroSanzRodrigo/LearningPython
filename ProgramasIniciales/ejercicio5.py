1310221abf9a77cdc01123e1835796e920400aff
Pablo Guardado Alvarez <pablo.guardado@live.u-tad.com>
2019-01-11 11:00:16 +0100

    ejercio 5 actualizado

--- ProgramasIniciales/ejercicio5.py
+++ ProgramasIniciales/ejercicio5.py
@@ -1,16 +1,27 @@
 
 
 salario = int(input("ingrese su sueldo mensual\n"))
+salarioFinal = 0
 
 if(salario>0 and salario<12450):
     print("19%")
+    salarioFinal = salario*19/100
+    print(salarioFinal)
 elif(salario>12450 and salario<20200):
     print("24%")
+    salarioFinal = salario*24/100
+    print(salarioFinal)
 elif(salario>20200 and salario<35200):
     print("30%")
+    salarioFinal = salario*30/100
+    print(salarioFinal)
 elif(salario>35200 and salario<60000):
     print("37%")
+    salarioFinal = salario*37/100
+    print(salarioFinal)
 elif(salario>60000):
     print("45%")
+    salarioFinal = salario*45/100
+    print(salarioFinal)
 
 
